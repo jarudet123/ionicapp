2d9c99cbc3f1eb68e885ed97aae335b1dde77692
Jarudet Chanin <alexx00008888@gmail.com>
2020-02-14 11:47:32 +0700

    1.0.3

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
 
+  name: string;
+  lastname: string;
+  detail: string;
+
+  profiles: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.loaddata();
+    
   }
 
+  delete_data(id: any){
+    console.log(id);
+    axios.get('http://localhost/ionicserver/delete_profile.php?id='+id)
+    .then(()=>{
+      console.log("ลบข้อมูลเรียบร้อย");
+      this.loaddata();
+    });
+  }
+
+  loaddata(){
+    axios.get('http://localhost/ionicserver/get_profiles.php').then((response) => {
+      //handle success
+        console.log(response.data);
+
+          this.profiles = response.data;
+        // this.name = response.data[1].name;
+        // this.lastname = response.data[1].lastname;
+        // this.detail = response.data[1].detail;
+        
+      })
+    
+      .catch((error) => {
+        // handle error
+          console.log(error);
+      })
+      .then(() => {
+        //always executed
+      });
+  }
 }
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  name: string;
+  lastname: string;
+  detail: string;
+
+  input_name: string;
+  input_lastname: string;
+  input_detail: string;
+
   constructor() { }
 
   ngOnInit() {
+    axios.get('http://localhost/ionicserver/get_profiles.php').then((response) => {
+      //handle success
+        console.log(response.data);
+        this.name = response.data[0].name;
+        this.lastname = response.data[0].lastname;
+        this.detail = response.data[0].detail;
+
+      })
+      .catch((error) => {
+        // handle error
+          console.log(error);
+      })
+      .then(() => {
+        //always executed
+      });
+  }
+
+  register(){
+    console.log('input_name: ' + this.input_name);
+    console.log('input_lastname: ' + this.input_lastname);
+    console.log('input_detail: ' + this.input_detail);
+
+    axios.post('http://localhost/ionicserver/register_profiles.php',{
+      name: this.input_name,
+      lastname: this.input_lastname,
+      detail: this.input_detail
+    }).then((response) => {
+      console.log(response);
+      this.input_name='';
+      this.input_lastname='';
+      this.input_detail='';
+    }).catch((error) => {
+      console.log(error);
+    });
+
+    
   }
 
 }
